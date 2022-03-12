a771384851641425f0a38973014d17b6dab5c929
Movses <68658828+mirmozavr@users.noreply.github.com>
2022-03-12 20:59:23 +0300

    Task 01, postman (#1)
    * Task 01, postman
    
    * Postman returns to base

---
+++ task01/task01.py
@@ -0,0 +1,48 @@
+from itertools import permutations
+
+post_office = (0, 2)
+waypoints = [
+    (2, 5),
+    (5, 2),
+    (6, 6),
+    (8, 3),
+]
+
+
+def calc_routes(points: list[tuple], post_office: tuple) -> tuple:
+    """Calculates shortest possible distance and returns
+    all paths of shortest distance, shortest distance and
+    a dict of distances between points."""
+    edges = {
+        (a, b): {
+            (x, y): ((a - x) ** 2 + (b - y) ** 2) ** 0.5 for x, y in [post_office] + waypoints
+        }
+        for a, b in [post_office] + waypoints
+    }
+    min_dist, min_paths = float("inf"), []
+
+    for path in permutations(points):
+        path = (post_office,) + path + (post_office,)
+        dist = sum((edges[path[i]][path[i + 1]] for i in range(len(path) - 1)))
+
+        if dist < min_dist:
+            min_dist = dist
+            min_paths = [path]
+        elif dist == min_dist:
+            min_paths.append(path)
+    return min_paths, min_dist, edges
+
+
+def path_printer(path: tuple, edges: dict) -> None:
+    accum_dist = 0
+    print('# ' * 25)
+    for i in range(len(path) - 1):
+        accum_dist += edges[path[i]][path[i + 1]]
+        print(f"{path[i]} -> {path[i + 1]}: [{accum_dist}]")
+    print(f"Total distance = {accum_dist}\n")
+
+
+if __name__ == '__main__':
+    paths, shortest_distance, edges = calc_routes(waypoints, post_office)
+    for path in paths:
+        path_printer(path, edges)
